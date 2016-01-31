cf293b36b1d47c7c33c713254ac6edc1e2dcd0e1
josephwithay <yoseph_jo@hotmail.com>
2016-01-31 01:09:18 -0800

    Indents instead of spaces in last/welcome and in Welcome::shucks() removed Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/controllers/last/Welcome.php
+++ application/controllers/last/Welcome.php
@@ -24,11 +24,9 @@ class Welcome extends Application {
 
 	function index()
 	{
-		$this->data['pagebody'] = 'justone';	// this is the view we want shown     
-                
-                $record = $this->quotes->last();
-                $this->data = array_merge($this->data, $record);
-
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		$record = $this->quotes->last();
+		$this->data = array_merge($this->data, $record);
 		$this->render();
 	}
 
